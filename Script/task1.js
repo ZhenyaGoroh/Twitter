2ca29dfca4d390c0d23eff7898e980a464a26d79
ZhenyaGoroh <zhenyagoroh@gmail.com>
2022-08-06 15:36:58 +0300

    func get Tweets without hashtags

---
+++ Script/task1.js
@@ -0,0 +1,192 @@
+
+
+const tweets = [
+  {
+    id: "1",
+    text: "Привет!",
+    createdAt: new Date("2022-03-09T23:00:00"),
+    author: "Иванов Иван",
+    comments: [],
+  },
+  {
+    id: "2",
+    text: "Как дела?",
+    createdAt: new Date("2022-03-09T23:00:01"),
+    author: "Петров Петр",
+    comments: [
+      {
+        id: "21",
+        text: "Хорошо, а у тебя?",
+        createdAt: new Date("2022-03-09T23:00:05"),
+        author: "Иванов Иван",
+      },
+    ],
+  },
+  {
+    id: "3",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:10:00"),
+    author: "Андреев Давид",
+    comments: [],
+  },
+  {
+    id: "4",
+    text: "Зачем нужен модуль?",
+    createdAt: new Date("2022-03-09T23:10:50"),
+    author: "Андреев Давид",
+    comments: [
+      {
+        id: "22",
+        text: "Модуль при помощи замыканий – это оборачивание пакета функциональности в единую внешнюю функцию, которая тут же выполняется.",
+        createdAt: new Date("2022-03-09T23:15:05"),
+        author: "Dolores Sanchez",
+      },
+    ],
+  },
+  {
+    id: "5",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:20:00"),
+    author: "Алексеева Полина",
+    comments: [],
+  },
+  {
+    id: "6",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:30:00"),
+    author: "Robert Guzman",
+    comments: [],
+  },
+  {
+    id: "7",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:31:00"),
+    author: "Wayne Martin",
+    comments: [
+      {
+        id: "23",
+        text: "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+        createdAt: new Date("2022-03-09T23:32:05"),
+        author: "Dolores",
+      },
+    ],
+  },
+  {
+    id: "8",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:35:10"),
+    author: "Сергей",
+    comments: [],
+  },
+  {
+    id: "9",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:40:44"),
+    author: "Женя",
+    comments: [],
+  },
+  {
+    id: "10",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:52:00"),
+    author: "Михаил Соловьев",
+    comments: [],
+  },
+  {
+    id: "11",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-10T03:00:50"),
+    author: "Ирина Лаптева",
+    comments: [
+      {
+        id: "24",
+        text: "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+        createdAt: new Date("2022-03-09T23:15:05"),
+        author: "Dolores Sanchez",
+      },
+    ],
+  },
+  {
+    id: "12",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-10T03:10:00"),
+    author: "Дмитрий Лобанов",
+    comments: [],
+  },
+  {
+    id: "13",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-10T03:22:53"),
+    author: "Даниил Тимофеев",
+    comments: [],
+  },
+  {
+    id: "14",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-09T23:10:00"),
+    author: "Андреев Давид",
+    comments: [],
+  },
+  {
+    id: "15",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-19T11:10:00"),
+    author: "Ян",
+    comments: [],
+  },
+  {
+    id: "16",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-12T23:10:00"),
+    author: "Никита",
+    comments: [
+      {
+        id: "25",
+        text: "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+        createdAt: new Date("2022-03-09T23:15:05"),
+        author: "Dolores Sanchez",
+      },
+      {
+        id: "26",
+        text: "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+        createdAt: new Date("2022-03-09T23:15:05"),
+        author: "Dolores Sanchez",
+      },
+    ],
+  },
+  {
+    id: "17",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-03-18T13:40:50"),
+    author: "Олег",
+    comments: [],
+  },
+  {
+    id: "18",
+    text: "Python",
+    createdAt: new Date("2022-03-091T12:11:00"),
+    author: "Mihnosha",
+    comments: [
+      {
+        id: "28",
+        text: "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+        createdAt: new Date("2022-03-09T23:15:05"),
+        author: "Dolores Sanchez",
+      },
+    ],
+  },
+  {
+    id: "19",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-04-09T23:10:00"),
+    author: "Nata",
+    comments: [],
+  },
+  {
+    id: "20",
+    text: "Если смогу, я сделаю это. Конец истории.",
+    createdAt: new Date("2022-04-21T14:11:11"),
+    author: "Zhenya",
+    comments: [],
+  },
+];
---
+++ Script/task2.js
@@ -0,0 +1,12 @@
+(function () {
+  let user;
+  function getTweets(skip = 0, top = 10, filterConfig) {
+    if(typeof skip == "object"){filterConfig=skip; skip=0};
+    let getTweetsArr = tweets.slice(skip, skip + top).sort((a, b) => a.createdAt - b.createdAt);
+    return filterConfig == undefined? getTweetsArr: getTweetsArr.filter((obj) => obj[Object.keys(filterConfig)[0]] == Object.values(filterConfig)[0]);
+  }
+  
+})();
+
+
+
